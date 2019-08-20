abb175a830bc0ffe19ba12c5cb7b17b04b8e18fe
Dayron <dvdayron@gmail.com>
2019-08-20 15:26:48 -0400

    cambios en el grid

---
+++ Agenciapp/Agenciapp/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Agenciapp.Models
+{
+    public partial class Order
+    {
+        public Guid OrderId { get; set; }
+        public Guid AgencyId { get; set; }
+        public Guid OfficeId { get; set; }
+        public Guid ClientId { get; set; }
+        public string Type { get; set; }
+        public string Number { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public decimal PriceLb { get; set; }
+        public decimal CantLb { get; set; }
+        public Guid? ValorAduanalId { get; set; }
+
+        public Agency Agency { get; set; }
+        public Client Client { get; set; }
+        public Office Office { get; set; }
+        public ValorAduanal ValorAduanal { get; set; }
+        public Package Package { get; set; }
+    }
+}
---
+++ Agenciapp/Agenciapp/Models/Shipping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Agenciapp.Models
+{
+    public partial class Shipping
+    {
+        public Shipping()
+        {
+            ShippingItem = new HashSet<ShippingItem>();
+        }
+
+        public Guid PackingId { get; set; }
+        public Guid AgencyId { get; set; }
+        public Guid OfficeId { get; set; }
+        public Guid CarrierId { get; set; }
+
+        public Agency Agency { get; set; }
+        public Carrier Carrier { get; set; }
+        public Office Office { get; set; }
+        public ICollection<ShippingItem> ShippingItem { get; set; }
+    }
+}
